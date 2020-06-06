5f1279cbcec707302b68e09d042baec8f3b0c116
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-06-06 14:28:30 -0700

    Update readme

---
+++ bin/checkout_latest_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCS_VERSIONS=(
+  master
+)
+
+for v in "${DOCS_VERSIONS[@]}"; do
+    if [ -d "resources/docs/$v" ]; then
+        echo "Pulling latest documentation updates for $v..."
+        (cd resources/docs/$v && git pull)
+    else
+        echo "Cloning $v..."
+        git clone --single-branch --branch "$v" git@github.com:ghost-language/docs.git "resources/docs/$v"
+    fi;
+done
