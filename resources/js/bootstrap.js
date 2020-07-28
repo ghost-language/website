8ab3e8ef8d6884fe6c4237cf4380a0a208c7267d
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-07-27 20:32:34 -0700

    Remove dead code

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,8 +4,4 @@ require('prismjs')
 require('prismjs/components/prism-go')
 require('prismjs/components/prism-dart')
 
-// const loadLanguages = require('prismjs/components/');
-
-// loadLanguages(['go']);
-
 import 'alpinejs'
