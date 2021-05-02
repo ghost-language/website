7fbf6ffabe294898efc9e76adcc11a4e0f96ecb8
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-05-02 12:29:30 -0700

    Add polyfill for wasm support

--- resources/js/ghost.js
+++ resources/js/ghost.js
@@ -1,3 +1,11 @@
+if (!WebAssembly.instantiateStreaming) {
+    WebAssembly.instantiateStreaming = async (response, importObject) => {
+        const source = await (await response).arrayBuffer();
+
+        return await WebAssembly.instantiate(source, importObject);
+    };
+}
+
 const go = new Go();
 
 WebAssembly.instantiateStreaming(fetch("/js/ghost.wasm"), go.importObject).then((result) => {
