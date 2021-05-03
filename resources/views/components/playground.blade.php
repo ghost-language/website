a3fac62747725282a175ce8925a7fbbf598d553b
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-05-03 10:08:46 -0700

    Clean up google tags

--- resources/views/components/playground.blade.php
+++ resources/views/components/playground.blade.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165345570-1"></script>
-
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-165345570-1');
-        </script>
+        <x-google />
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
