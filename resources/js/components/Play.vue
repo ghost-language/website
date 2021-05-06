068ccf4896e1292e062b9c36e976898b90e5b33c
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-05-06 14:10:52 -0700

    Update play styles

--- resources/js/components/Play.vue
+++ resources/js/components/Play.vue
@@ -4,19 +4,21 @@
             <pane>
                 <splitpanes>
                     <pane key="editor">
-                        <div class="flex items-center flex-none pl-5 pr-4 sm:pl-6 absolyte z-10 top-0 left-0 py-3 bg-gray-800 text-gray-100 border-b border-gray-700">
-                            <div class="flex space-x-5">
-                                <button @click.prevent="run" type="button" class="flex items-center text-sm font-medium focus:outline-none text-gray-100">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 -ml-0.5 h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                                    </svg>
+                        <div class="flex h-full flex-col-reverse lg:flex-row">
+                            <nav aria-label="sidebar" class="block flex-shrink-0 bg-gray-800 overflow-y-auto">
+                                <div class="relative w-14 flex flex-col p-3 space-y-3">
+                                    <a href="#" @click.prevent="run" class="text-gray-400 hover:bg-gray-700 hover:text-gray-100 flex-shrink-0 inline-flex items-center justify-center h-8 w-8 rounded-lg">
+                                        <span class="sr-only">Archive</span>
+                                        <!-- Heroicon name: outline/archive -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </nav>
 
-                                    Run
-                                </button>
-                            </div>
+                            <textarea ref="editor" v-model="code" rows="14" class="w-full h-full sm:text-sm p-4 font-mono bg-gray-800 border-0 text-gray-300 focus:outline-none focus:ring-0 ring-transparent" style="resize: none;"></textarea>
                         </div>
-
-                        <textarea ref="editor" v-model="code" rows="14" class="block w-full h-full sm:text-sm p-4 font-mono bg-gray-800 border-0 text-gray-300 focus:outline-none focus:ring-0 ring-transparent" style="resize: none;"></textarea>
                     </pane>
 
                     <pane key="results" v-if="!isMobile">
