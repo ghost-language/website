bd283b5d022f93801f4be84cb6939d122f411ae1
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2021-09-12 01:16:43 -0700

    Update vanity list

--- config/ghost.php
+++ config/ghost.php
@@ -4,8 +4,11 @@ return [
 
     'repositories' => [
 
-        'engine' => 'https://github.com/ghost-language/engine',
-        'ghost'  => 'https://github.com/ghost-language/ghost',
+        'engine'  => 'https://github.com/ghost-language/engine',
+        'ghost'   => 'https://github.com/ghost-language/ghost',
+        'parser'  => 'https://github.com/ghost-language/parser',
+        'scanner' => 'https://github.com/ghost-language/scanner',
+        'token'   => 'https://github.com/ghost-language/token',
 
     ]
 
